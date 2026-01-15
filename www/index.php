546adbdfb67eccd25975a5a907ff6640fe6db773
Kodys <kodys@arit.cz>
2026-01-15 09:54:54 +0100

    cache na slugz clanku pri boot aplikace.

--- www/index.php
+++ www/index.php
@@ -2,9 +2,22 @@
 
 declare(strict_types=1);
 
+use App\Repository\ArticleRepository;
+use Nette\Caching\Cache;
+use Nette\Caching\Storages\FileStorage;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $bootstrap = new App\Bootstrap;
 $container = $bootstrap->bootWebApplication();
+
+$storage = new FileStorage(__DIR__ . '/../temp/cache');
+$cache = new Cache($storage);
+$articleRepository = $container->getByType(ArticleRepository::class);
+
+if ($cache->load(ArticleRepository::ALL_ARTICLE_SLUGS_CACHE_KEY) === null) {
+	$cache->save(ArticleRepository::ALL_ARTICLE_SLUGS_CACHE_KEY, $articleRepository->getAllSlugs(), [Cache::Expire => '10 minutes']);
+}
+
 $application = $container->getByType(Nette\Application\Application::class);
 $application->run();
